bcf67e5350df9e574999824d3926ee0a03c047d6
Mihail <planetared@yandex.ru>
2025-12-19 07:55:07 +0700

    Made feature1, f1 and f2

--- 5_3.py
+++ 5_3.py
@@ -10,3 +10,33 @@ def print_functions_menu():
     print("4. Поменять местами первый и последний столбец")
     print("5. Вычислить сумму положительных элементов в строках без нулевых элементов")
     print("6. Назад")
+
+def function_1():
+    if array is None:
+        print("Массив не создан!")
+        return
+    
+    print("Индексы отрицательных элементов:")
+    found = False
+    for i in range(rows):
+        for j in range(cols):
+            if array[i][j] < 0:
+                print(f"[{i}][{j}] = {array[i][j]}")
+                found = True
+    if not found:
+        print("Отрицательных элементов нет")
+
+def function_2():
+    if array is None:
+        print("Массив не создан!")
+        return
+    
+    print("Элементы, кратные 5, и их индексы:")
+    found = False
+    for i in range(rows):
+        for j in range(cols):
+            if array[i][j] % 5 == 0:
+                print(f"[{i}][{j}] = {array[i][j]}")
+                found = True
+    if not found:
+        print("Элементов, кратных 5, нет")
